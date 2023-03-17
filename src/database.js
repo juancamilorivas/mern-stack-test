c02546a55477635868d86bab3e5bbef44a8b1be3
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2023-03-17 17:19:57 -0500

    first backend

---
+++ src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const URI = 'mongodb://localhost/mernstack';
+
+mongoose.connect(URI, {    
+});
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+    console.log('DB is connected');
+});
