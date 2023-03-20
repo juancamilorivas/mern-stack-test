814d351a304405caf98a91231f7b45e6c5f3fefb
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2023-03-20 09:53:37 -0500

    Some methods were created in routes/user

--- src/routes/user.js
+++ src/routes/user.js
@@ -39,4 +39,14 @@ router.put("/users/:id", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+
+//Delete user
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;
